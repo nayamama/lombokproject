18d5e4c6a0ced82b8be80f6705aa4c4536fa80ac
Qi Yang <nayamama@hotmail.com>
2019-07-28 22:31:38 -0400

    Initial commit

---
+++ src/main/java/com/my_java/model/User.java
@@ -0,0 +1,17 @@
+package com.my_java.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
+
